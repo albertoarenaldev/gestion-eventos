1ad06078ef27b67e37b45dcde83d7c512e74bf73
alberto <noname@nomaillocal.com>
2025-09-08 18:35:03 +0200

    Cambios en entidad TipoEvento, añadidos repositorios de Evento y TipoEvento y añadido controller TipoEventoController

---
+++ back/src/main/java/es/cic/curso25/back/TipoEventoController.java
@@ -0,0 +1,12 @@
+package es.cic.curso25.back;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tipo_evento")
+public class TipoEventoController {
+
+
+
+}
--- back/src/main/java/es/cic/curso25/back/modelo/TipoEvento.java
+++ back/src/main/java/es/cic/curso25/back/modelo/TipoEvento.java
@@ -2,7 +2,6 @@ package es.cic.curso25.back.modelo;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
