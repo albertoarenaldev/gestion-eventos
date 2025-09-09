facbc4f666f4994d0f7114819df6fbe921a21f94
alberto <noname@nomaillocal.com>
2025-09-09 17:02:08 +0200

    Excepción personalizada nombre existente TipoEvento, desarrolllo TipoEventoService y creado testCreate()  de integración de TipoEvento

--- back/src/main/java/es/cic/curso25/back/services/TipoEventoService.java
+++ back/src/main/java/es/cic/curso25/back/services/TipoEventoService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import es.cic.curso25.back.controlleradvice.TipoEventoEmptyNameException;
 import es.cic.curso25.back.modelo.TipoEvento;
 import es.cic.curso25.back.repository.TipoEventoRepository;
 
@@ -22,7 +23,43 @@ public class TipoEventoService {
 
     @Transactional(readOnly = true)
     public List<TipoEvento> findAll() {
+
+        LOGGER.info("Buscando todos los tipos de eventos");
         return tipoEventoRepository.findAll();
     }
 
+    public TipoEvento create(TipoEvento tipoEvento) {
+
+        LOGGER.info("Creando un nuevo tipo de evento");
+
+        comprobacionNombre(tipoEvento);
+        return tipoEventoRepository.save(tipoEvento);
+    }
+
+    
+
+    public TipoEvento update(TipoEvento tipoEvento) {
+
+        LOGGER.info("Actualizando el tipo de evento con id: {}", tipoEvento.getId());
+        return tipoEventoRepository.save(tipoEvento);
+    }
+
+    public void delete(Long id) {
+
+        LOGGER.info("Eliminando el tipo de evento con id: {}", id);
+        tipoEventoRepository.deleteById(id);
+    }
+
+    private void comprobacionNombre(TipoEvento tipoEvento) {
+        
+        if (tipoEvento.getNombre() == null || tipoEvento.getNombre().isEmpty()) {
+            throw new TipoEventoEmptyNameException("El nombre no puede ser nulo o vacío");
+        } else {
+            // comprobar que no existe otro tipo de evento con el mismo nombre
+            List<TipoEvento> tipos = tipoEventoRepository.findByNombre(tipoEvento.getNombre());
+            if (!tipos.isEmpty()) {
+                throw new IllegalArgumentException("Ya existe un tipo de evento con el nombre: " + tipoEvento.getNombre());
+            }
+        }
+    }
 }
